1aae68f5332bbe5d0e4eafee16a0c2b119663142
redfoxforever <pubgbot410@gmail.com>
2022-06-13 13:15:17 +0300

    ver 1.0

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ fetch(url)
     if (response.ok) return response.json()
 })
 .then(data => {
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
         let div = document.createElement('div')
         div.classList.add('card')
         div.innerHTML = `
-        <h2 class="card-title">Image ${i + 1} </h2>
+        <h2 class="card-title">Image ${data[i].id}</h2>
         <img src="${data[i].url}" alt="" class="card-img">
         `
         cards.append(div)
